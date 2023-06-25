b87fe6521407fb4dd6429e93a586235aa82d8921
kpokpetreasure <137432864+kpokpetreasure@users.noreply.github.com>
2023-06-25 07:33:30 +0100

    update

--- GameMode.js
+++ GameMode.js
@@ -1,21 +1,22 @@
 "use strict";
 
 let ran = Math.trunc(Math.random() * 10) + 1;
-let trial = 10;
+let trial = 3;
+let myst = document.querySelector(".myst");
 
 document.querySelector(".dif").addEventListener("click", function () {
   ran = Math.trunc(Math.random() * 1058765936296260622566704) + 1;
   console.log(ran);
   document.querySelector(".p").textContent =
     "Terror, it is impossible to win HaHahahahahaha";
-  document.querySelector(".myst").value = "";
+  myst.value = "";
 });
 
 function logic() {
   if (trial > -1) {
     document.querySelector(".Trial").textContent = trial--;
     if (Number(document.querySelector(".myst").value) === ran) {
-      document.querySelector(".p").textContent = "Correct";
+      document.querySelector(".p").textContent = "Correct, You WON!!";
       document.querySelector("html").style.backgroundColor =
         "rgb(255, 247, 16)";
       trial = "";
@@ -35,9 +36,33 @@ document.querySelector(".guess").addEventListener("click", logic);
 
 document.querySelector(".agn").addEventListener("click", function () {
   ran = Math.trunc(Math.random() * 10) + 1;
-  trial = 10;
+  trial = 3;
   document.querySelector(".Trial").textContent = trial;
   document.querySelector(".p").textContent = "0";
   document.querySelector(".myst").value = "";
   document.querySelector("html").style.backgroundColor = "blueviolet";
 });
+
+function logic1(rema) {
+  if (rema.keyCode == 13) {
+    if (trial > -1) {
+      document.querySelector(".Trial").textContent = trial--;
+      if (Number(document.querySelector(".myst").value) === ran) {
+        document.querySelector(".p").textContent = "Correct,  You WON!!";
+        document.querySelector("html").style.backgroundColor =
+          "rgb(255, 247, 16)";
+        trial = "";
+      } else if (Number(document.querySelector(".myst").value) < ran) {
+        document.querySelector(".p").textContent = "Too low";
+      } else if (Number(document.querySelector(".myst").value) > ran) {
+        document.querySelector(".p").textContent = "Too high";
+      } else if ((trial = 1)) {
+        document.querySelector("html").style.backgroundColor = "gold";
+      }
+    } else {
+      document.querySelector(".p").textContent = "Game over";
+    }
+  }
+}
+
+myst.addEventListener("keypress", logic1);
