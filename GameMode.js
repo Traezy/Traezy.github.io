932d5b51dc845b65a7fbb427242b287796bb75f9
kpokpetreasure <137432864+kpokpetreasure@users.noreply.github.com>
2023-06-27 03:04:26 +0100

    Game preference

--- GameMode.js
+++ GameMode.js
@@ -3,11 +3,10 @@
 let ran = Math.trunc(Math.random() * 10) + 1;
 let trial = 3;
 let myst = document.querySelector(".myst");
-let amt = document.querySelector(".amt");
-let amnt = 0;
 let hc = document.querySelector("html").style.background;
+console.log(ran);
 document.querySelector(".dif").addEventListener("click", function () {
-  ran = Math.trunc(Math.random() * 1058765936296260622566704) + 1;
+  ran = Math.trunc(Math.random() * 105876593629613924) + 1;
   document.querySelector(".p").textContent =
     "Terror, it is impossible to win HaHahahahahaha";
   myst.value = "";
@@ -15,13 +14,8 @@ document.querySelector(".dif").addEventListener("click", function () {
 });
 
 function logic() {
-  if (myst.value == ran && trial == 3) {
-    amt.textContent = Number(amnt) + 1;
-    document.querySelector("html").style.backgroundColor = "rgb(255, 247, 16)";
-    document.querySelector(".p").textContent = "Correct, You WON!!";
-  }
   ///////////////////////////
-  else if (trial > -1) {
+  if (trial > -1) {
     document.querySelector(".Trial").textContent = trial--;
     if (Number(document.querySelector(".myst").value) === ran) {
       document.querySelector("html").style.backgroundColor =
@@ -72,18 +66,4 @@ document.querySelector(".agn").addEventListener("click", function () {
   document.querySelector(".p").textContent = "?";
   document.querySelector(".myst").value = "";
   document.querySelector("html").style.backgroundColor = "blueviolet";
-});
-
-myst.addEventListener("mouseleave", function () {
-  if (amt.textContent == 20) {
-    let io = `$20 is max for now. endeavour to check in another time `;
-    amt.textContent = io;
-    amt.style.display = "none";
-  } else if (myst.value == ran && trial == 3) {
-    amt.textContent = amnt++;
-  } else if (amt.textContent == 2) {
-    amt.textContent = 3;
-  } else if (io === `$20 is max for now. endeavour to check in another time `) {
-    amt.style.display = "none";
-  }
 });
